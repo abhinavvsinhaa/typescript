13fbe412d70592f52d3db1119dec9d0678ea3d26
Abhinav Sinha <abhinavvsinhaa@gmail.com>
2023-07-25 12:06:39 +0530

    object and variable type annotations

---
+++ 01 - type annotations in action/variable.ts
@@ -0,0 +1,26 @@
+let apples: number = 5; // type annotation
+let speed: string = 'fast';
+let hasName: boolean = true;
+
+// value same as types
+let x: undefined = undefined;
+let y: null = null;
+
+// built in objects
+let now: Date = new Date();
+
+// array
+let colors: string[] = ['red', 'green', 'black'];
+let arr: number[] = [3, 23, 2];
+let hasIt: boolean[] = [true, false];
+
+// class
+class Car {}
+
+let car: Car = new Car();
+
+// object literal
+let point: { x: number; y: number } = {
+  x: 10,
+  y: 20,
+};
---
+++ README.md
@@ -0,0 +1,27 @@
+## Types in TypeScript
+
+### Primitive types
+
+- number
+- boolean
+- string
+- void
+- undefined
+- null
+
+### Object types
+
+- functions
+- arrays
+- classes
+- objects
+
+## Type annotation & inferences
+
+### Type annotation
+
+We specify TS, what type of value a variable will refer to
+
+### Type inference
+
+TS figures out what type of value a variable refers to
